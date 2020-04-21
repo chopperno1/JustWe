3337a4c9d7237d0618f5bb5b63061742cf3045af
Just_we_home <chopperno1@163.com>
2020-04-21 17:01:47 +0800

    修改测试类包路径

--- src/test/java/com/JustWe/JustWeApplicationTests.java
+++ src/test/java/com/JustWe/JustWeApplicationTests.java
@@ -1,13 +1,21 @@
-package com.JustWeDemo;
+package com.JustWe;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JustWeApplicationTests {
 
+	Logger logger = LoggerFactory.getLogger(getClass());
+
 	@Test
 	void contextLoads() {
+
+		logger.info("测试测试");
+
+
 	}
 
 }
